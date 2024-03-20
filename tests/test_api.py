f084f12053c9995826bc76d4ecf24b4bbd50dff8
Andy Xie <andy.xie.opensource@gmail.com>
2024-03-20 18:21:06 +0800

    test_api.py: Specify args

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,13 +6,13 @@ from leetcode_0088_merge_sorted_array import api
 
 
 @pytest.mark.parametrize(
-    ["result", ...],
+    ["result", "nums1", "m", "nums2", "n"],
     (
         [..., ...],
         [..., ...],
     )
 )
-def test_merge_sorted_array(result, ...) -> None:
+def test_merge_sorted_array(result, nums1, m, nums2, n) -> None:
     """Tests solution for problem Merge Sorted Array"""
 
-    assert api.merge_sorted_array(...) == result
+    assert api.merge_sorted_array(nums1, m, nums2, n) == result
