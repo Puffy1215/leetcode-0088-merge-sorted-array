099501ae9075f8d2e33b23f3675b0905c0609ee2
Andy Xie <andy.xie.opensource@gmail.com>
2024-03-20 18:57:30 +0800

    test_api.py: Add test data

--- tests/test_api.py
+++ tests/test_api.py
@@ -8,9 +8,10 @@ from leetcode_0088_merge_sorted_array import api
 @pytest.mark.parametrize(
     ["result", "nums1", "m", "nums2", "n"],
     (
-        [..., ...],
-        [..., ...],
-    )
+        [[1, 2, 2, 3, 5, 6], [1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3],
+        [[1], [1], 1, [], 0],
+        [[1], [0], 0, [1], 1],
+    ),
 )
 def test_merge_sorted_array(result, nums1, m, nums2, n) -> None:
     """Tests solution for problem Merge Sorted Array"""
