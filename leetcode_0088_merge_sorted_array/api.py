b3f6069d75d1da4b23689dc22fa7565c14fb14a6
Andy Xie <andy.xie.opensource@gmail.com>
2024-03-22 21:14:42 +0800

    api.py: Specify args for merge_sorted_array

--- leetcode_0088_merge_sorted_array/api.py
+++ leetcode_0088_merge_sorted_array/api.py
@@ -6,32 +6,33 @@ LEN_MIN = 0
 NUM_MAX = 10**9
 NUM_MIN = -NUM_MAX
 
+
 def _check_preconditions(nums1: list[int], m: int, nums2: list[int], n: int) -> bool:
     if not len(nums1) == m + n:
         return False
-    
+
     if not len(nums2) == n:
         return False
-    
+
     for l in [m, n]:
         if not LEN_MIN <= l <= LEN_MAX:
             return False
-    
+
     if not LEN_MIN + 1 <= m + n <= LEN_MAX:
         return False
-    
+
     for nums, l in [(nums1, m), (nums2, n)]:
         for x in nums:
             if not NUM_MIN <= x <= NUM_MAX:
                 return False
-        
+
         if sorted(nums[:l]) != nums:
             return False
-    
+
     return True
 
 
-def merge_sorted_array(...) -> ...:
+def merge_sorted_array(nums1: list[int], m: int, nums2: list[int], n: int) -> None:
     """Solves problem Merge Sorted Array"""
 
     assert _check_preconditions(...)
