0166b8f473ec7074974f973e4a7e21e1906c5566
Andy Xie <andy.xie.opensource@gmail.com>
2024-03-31 21:27:45 +0800

    test_api.py: Impement nums_rand() test

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,5 +1,8 @@
 """Tests API for solving problem Merge Sorted Array"""
 
+import random
+from typing import Callable
+
 import pytest
 
 from leetcode_0088_merge_sorted_array import api
@@ -22,3 +25,19 @@ def test_merge_sorted_array(
     api.merge_sorted_array(nums1, m, nums2, n)
 
     assert nums1 == result
+
+
+@pytest.fixture
+def nums_rand() -> Callable[[], tuple[list[int]]]:
+    """Fixture to generate random x"""
+
+    def _nums_rand() -> tuple[list[int]]:
+        l = random.randint(api.LEN_MIN + 1, api.LEN_MAX)
+        m = random.randint(api.LEN_MIN, l)
+        n = l - m
+        nums = [random.randint(api.NUM_MIN, api.NUM_MAX) for _ in range(l)]
+        nums1 = sorted(nums[:m]) + [0] * n
+        nums2 = sorted(nums[m:])
+        return nums1, nums2
+
+    return nums_rand
